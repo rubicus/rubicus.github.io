be6390f8624fad11ef84c9196066ded2e25b30f1
Oskar Södergren <oskso569@student.liu.se>
2016-09-25 01:11:19 +0200

    Wrote a script for adding new tag index pages

---
+++ tag/add_tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script adds a new index page for a tag named by the first argument
+# to the script. This is done by copying the template folder and renaming
+# the tag variable in it.
+
+#Make sure we have an argument:
+if [ -z "$1" ]; then
+    echo usage: $0 TAG_NAME
+    exit
+fi
+
+# copy the folder:
+cp -R template "$1"
+
+# fix the tag name in index.html to the new tag name
+# serch for "changethis" and replace with argument
+sed -i -e "s/changethis/${1}/g" "$1/index.html"
+
+echo "Added an index page for the new tag '$1'"
